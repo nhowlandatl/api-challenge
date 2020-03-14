780219a2dcc4fb5c60a6ba9259472f77290abdcb
Nick Howland <nhowland@outlook.com>
2020-03-14 11:21:30 -0400

    added apiKey.js

---
+++ apiKey.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAWvxohWY9cxG_tvFPlhY9WyZLOF3i9T-0",
+    authDomain: "fir-project-84a1f.firebaseapp.com",
+    databaseURL: "https://fir-project-84a1f.firebaseio.com",
+    projectId: "fir-project-84a1f",
+    storageBucket: "fir-project-84a1f.appspot.com",
+    messagingSenderId: "431929357352",
+    appId: "1:431929357352:web:107d71d8bde150e6ad311d"
+  };
+
+
+  
